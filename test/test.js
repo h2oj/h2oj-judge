2fdf662ea3c082fa5e83c0e213858144857fea97
Alex Cui <cuizhihui030925@outlook.com>
2021-02-19 22:04:45 +0800

    Update test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const HojJudger = require('../node/index');
+
+HojJudger.judge({
+    code_path: './test/aplusb_problem/src.cpp',
+    problem_path: './test/aplusb_problem/data',
+    output_path: './test/aplusb_problem/',
+    language: 'cpp98'
+}).then(result => {
+    console.log(result);
+});
